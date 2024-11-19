f333870aa4142c6ffb69ab771afb748f628ed0f1
Miguel477713 <131222467+Miguel477713@users.noreply.github.com>
2024-11-19 09:02:27 -0600

    Add files via upload

---
+++ Has_a_Realtionship.cpp
@@ -0,0 +1,41 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class BoschTPMS{
+    public:
+BoschTPMS(){
+            cout<<"Bosch TPMS Constructed "<<endl;
+    }
+    ~BoschTPMS(){
+            cout<<"Bosch TPMS Destructed "<<endl;
+    }
+};
+class Engine{
+public:
+    Engine(){
+            cout<<"Engine Constructed "<<endl;
+    }
+    ~Engine(){
+            cout<<"Engine Destructed "<<endl;
+    }
+};
+class Car{
+        //Contains,composition , death relationship
+        Engine engineObj;
+        //holds,Aggregation
+        BoschTPMS * tpmsObj;
+        public:
+         Car(){
+            tpmsObj = new BoschTPMS();
+            cout<<"Car Constructed "<<endl;
+        }
+        ~Car(){
+            cout<<"Car Destructed "<<endl;
+    }
+};
+
+int main() {
+    // Write C++ code here
+    Car car;
+    return 0;
+}
